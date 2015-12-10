d76848cd67f793708fdf1f8d3ae1b9ef4e48e310
劉至峻 <Chih-Chun_Liu@liuzhijundeMacBook-Pro.local>
2015-12-10 20:49:29 +0800

    nav bar 樣式更改 浮筐登入

--- iLibrary/application/views/layout/header.php
+++ iLibrary/application/views/layout/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Posts</title>
+		<title>iLibrary</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
--- iLibrary/application/views/layout/navbar.php
+++ iLibrary/application/views/layout/navbar.php
@@ -13,7 +13,7 @@
 		</div>
 		<div class="collapse navbar-collapse" id="navbar">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="<?php echo site_url('post/index') ?>">圖書庫</a></li>
+				<li class="active"><a href="<?php echo site_url('account/index') ?>">圖書庫</a></li>
 				<li><a href="#">預約追蹤</a></li>
 				<li><a href="#">借書紀錄</a></li>
 				<li><a href="#">會員管理</a></li>
@@ -21,10 +21,46 @@
 				<li><a href="#">借還書管理</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Login</a></li>
-				<li><a href="#">Logout</a></li>
+				<?php if ($this->session->ssn): ?>
+					<li class="dropdown">
+          				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> 
+          					<?php echo $this->session->userdata('name') ?> <span class="caret"></span>
+          				</a>
+          				<ul class="dropdown-menu">
+            				<li><a href="<?php echo site_url('login/logout') ?>">logout</a></li>
+          				</ul>
+        			</li>
+				<?php else : ?>
+					<li><a href="#" data-toggle="modal" data-target=".login-modal">Login</a></li>
+				<?php endif ?>
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->
 </nav>
 <div style="padding-top:70px;"></div>
+
+<div class="modal login-modal" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-sm">
+	    <div class="modal-content">
+		    <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="model-title">Login</h4>
+		    </div>
+		    <form action="<?php echo site_url('login/login') ?>" method="POST">
+      			<div class="modal-body" id="modal-login-body">
+        			<div class="input-group">
+		            	<span class="input-group-addon">帳號</span>
+		            	<input type="text" class="form-control" name = 'e-mail'>
+		        	</div>
+		        	<div class="input-group">
+		            	<span class="input-group-addon" name = 'password'>密碼</span>
+		            	<input type="password" class="form-control"  name = 'password'>
+		        	</div>
+      			</div>
+      			<div class="modal-footer">
+		        	<button type="submit" class="btn btn-primary">登入</button>
+		    	</div>
+		    </form>
+    	</div><!-- /.modal-content -->
+  	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
