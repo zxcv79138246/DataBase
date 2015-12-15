06212de38b62c21c9e87dd875f83f014a46f8c8e
劉至峻 <Chih-Chun_Liu@liuzhijundeMacBook-Pro.local>
2015-12-15 17:59:24 +0800

    controller index 增加讀取所以書籍 all()

--- iLibrary/application/controllers/Index.php
+++ iLibrary/application/controllers/Index.php
@@ -9,12 +9,15 @@ class Index extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->model('book_model', 'book');
+
 	}
 
 	public function index(){
+		$book = $this->book->all();
 		$this->load->view('layout/header');
 		$this->load->view('layout/navbar');
-		$this->load->view('library/index', compact('library'));
+		$this->load->view('library/index', compact('book'));
 		$this->load->view('layout/footer');
 	}
 }
